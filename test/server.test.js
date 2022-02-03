c4ccbeccab68162d9ac28f32f9572dbfcee9b94b
James Speirs <james.speirs@gmail.com>
2022-02-02 18:30:49 -0700

    make test work

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect
 const app = require('../api/server')
-const request = require('supertest')(app)
+const request = require('supertest')
 
 describe('server', () => {
 
@@ -10,7 +10,7 @@ describe('server', () => {
             // Remember that one way to run asynchronous tests
             // is to return a promise.
             return request(app)
-                .post('/accounts')
+                .post('/api/accounts')
                 .send({
                     username: 'bob12',
                     name: 'Bob',

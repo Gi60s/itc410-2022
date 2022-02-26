f7e9becd830abd199635ef113004f9a41dd81df9
James Speirs <james.speirs@gmail.com>
2022-02-26 16:46:58 -0700

    use varchar and add sessions table

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,18 +1,34 @@
+-- Accounts
+
 CREATE TABLE "accounts" (
-  "account_id" character(36) NOT NULL,
-  "username" character(40) NOT NULL,
-  "name" character(80) NOT NULL,
-  "password" character(80) NOT NULL,
+  "account_id" varchar(36) NOT NULL,
+  "username" varchar(40) NOT NULL,
+  "name" varchar(80) NOT NULL,
+  "password" varchar(80) NOT NULL,
   PRIMARY KEY ("account_id")
 );
 
 ALTER TABLE "accounts"
 ADD CONSTRAINT "accounts_username" UNIQUE ("username");
 
+-- Sessions
+
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
+-- Tasks
+
 CREATE TABLE "tasks" (
-  "task_id" character(36) NOT NULL,
-  "account_id" character(36) NOT NULL,
-  "title" character(80) NOT NULL,
+  "task_id" varchar(36) NOT NULL,
+  "account_id" varchar(36) NOT NULL,
+  "title" varchar(80) NOT NULL,
   "description" text NOT NULL,
   "due_date" timestamptz NOT NULL,
   "completed" timestamptz NOT NULL,

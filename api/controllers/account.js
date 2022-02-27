95095596ddedecab2a2bacf7f187b1a38e7b8f67
James Speirs <james.speirs@gmail.com>
2022-02-26 17:02:13 -0700

    the authenticated user can only update and delete their own account

--- api/controllers/account.js
+++ api/controllers/account.js
@@ -17,16 +17,18 @@ module.exports = function (pool) {
 
 		async updateAccount (req, res) {
 			const data = req.enforcer.body
-			const { accountId } = req.enforcer.params
+			const { username } = req.enforcer.params
 
 			const client = await pool.connect()
 			try {
 				await client.query('BEGIN')
-				let account = await accounts.getAccount(client, accountId)
+				let account = await accounts.getAccountByUsername(client, username)
 				if (account === undefined) {
 					res.enforcer.status(404).send()
+				} else if (account.account_id !== req.user.id) {
+					res.enforcer.status(403).send()
 				} else {
-					await accounts.updateAccount(client, accountId, data)
+					await accounts.updateAccount(client, req.user.id, data)
 					res.enforcer.status(200).send()
 				}
 				await client.query('COMMIT')
@@ -39,9 +41,25 @@ module.exports = function (pool) {
 		},
 
 		async deleteAccount (req, res) {
-			const { accountId } = req.enforcer.params
-			await accounts.deleteAccount(pool, accountId)
-			res.enforcer.status(204).send()
+			const { username } = req.enforcer.params
+			try {
+				await client.query('BEGIN')
+				let account = await accounts.getAccountByUsername(client, username)
+				if (account === undefined) {
+					res.enforcer.status(204).send()
+				} else if (account.account_id !== req.user.id) {
+					res.enforcer.status(403).send()
+				} else {
+					await accounts.deleteAccount(pool, account.id)
+					res.enforcer.status(200).send()
+				}
+				await client.query('COMMIT')
+			} catch (e) {
+				await client.query('ROLLBACK')
+				throw e
+			} finally {
+				client.release()
+			}
 		},
 
 		async login (req, res) {
